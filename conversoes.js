0cd9aa87100122bba042e881e76ef1d5990f29f2
Adriano <150024041+Blockymage45@users.noreply.github.com>
2026-04-06 15:20:11 +0000

    dasdsads

--- conversoes.js
+++ conversoes.js
@@ -1,13 +1,10 @@
-//tipo de dados
-//booleanos
+// tipo de dado
+// booleamos
 
-// conversão implícita
+// conversão implicita
+const numero = 456
+const numeroString = "456";
 
-const numero = 456;
-const numeroString = '456';
+console.log(numero = numeroString)
 
-console.log(numero == numeroString);
-
-//conversão explícita
-
-console.log(numero + Number(numeroString));
+// conversão implicita
