72f00cb5b3d81a7baf0a6781cc01a18d2aabf879
Adriano <150024041+Blockymage45@users.noreply.github.com>
2026-04-06 15:21:05 +0000

    l,llll

--- conversoes.js
+++ conversoes.js
@@ -2,9 +2,10 @@
 // booleamos
 
 // conversão implicita
+
 const numero = 456
 const numeroString = "456";
 
-console.log(numero = numeroString)
+console.log(numero + Number(numeroString));
 
 // conversão implicita
